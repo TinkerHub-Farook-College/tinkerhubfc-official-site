168b1543fa503cc57a69c73f2ebd9dc59d3f14ec
rijfas <rijfas01@gmail.com>
2021-09-30 14:46:44 +0530

    refactor-1

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ function loadEvents(events_string, container, type) {
             ` : '';
             const eventCard = `
             <div class="xl:w-1/4 md:w-1/2 p-4">
-              <div class="flex flex-col justify-between bg-gray-100 p-6 rounded-lg h-full">
+              <div class="flex flex-col justify-between bg-gray-100 p-6 rounded-lg h-full wow fadeInUp">
                 <div>
                     <img class="h-40 rounded w-full object-cover object-center mb-6" src="events/assets/${event.image}" alt="content">
                     <h3 class="tracking-widest text-yellow-500 text-xs font-medium title-font">${event.date}</h3>
